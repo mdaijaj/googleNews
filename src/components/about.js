45c2a561178c5aebad86dce07d144aecaafc64bb
aijajkhan7 <aijaj@elitemindz.co>
2022-04-05 07:36:41 +0530

    add file

--- src/components/about.js
+++ src/components/about.js
@@ -4,17 +4,20 @@ const cors = require('cors')
 const axios = require('axios')
 
 
-const About = () => {
-    // const apiKey = "2f35351b15a94b7bab667e5b6f19f560";
-    // const country = "us";
-    // const baseUrl = `https://newsapi.org/v2/top-headlines?${country}=us&apiKey${apiKey}`
-    const headlineUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=2f35351b15a94b7bab667e5b6f19f560";
 
-    const userHome = async () => {
-        axios.get(`${headlineUrl}`)
+const Sports = () => {
+    const apiKey = "2f35351b15a94b7bab667e5b6f19f560";
+    const country = "us"
+    const category = "sports"
+    const baseUrl = `https://newsapi.org/v2/top-headlines`
+    const [userData, setData] = useState([]);
+
+    const sportPage = async () => {
+        axios.get(`${baseUrl}?country=${country}&category=${category}&apiKey=${apiKey}`)
             .then((res) => {
-                let data = res.data.articles;
+                let data = res.data.articles
                 console.log("data", data)
+                setData(data);
             })
             .catch(err => {
                 console.log(err.message)
@@ -22,17 +25,61 @@ const About = () => {
     }
 
     useEffect(() => {
-        userHome();
-    }, []);
+        sportPage();
+    });
+
+    let imgpro = {
+        height: "250px",
+        width: "300px"
+    }
+
+    // {
+    //     userData.map((element => {
+    //         return (
+    //             <>
+    //                 <div class="card">
+    //                     <img src={element.urlToImage} class="card-img-top" alt="..." />
+    //                     <div class="card-body">
+    //                         <h5 class="card-title">{element.url}</h5>
+    //                         <p class="card-text">{element.description}</p>
+    //                     </div>
+    //                     <div class="card-footer">
+    //                         <small class="text-muted">{element.publishedAt}</small>
+    //                     </div>
+    //                 </div>
+
+    //             </>
+    //         )
+    //     }))
+    // }
 
     return (
         <>
-        <h2> About pages</h2>
-        <div className="container">
-        </div>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                {
+                    userData.map((element => {
+                        return (
+                            <div class="col">
+                                <div class="card h-50">
+                                    <img src={element.urlToImage} class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">{element.author}</h5>
+                                        <p class="card-text">{element.content}</p>
+                                       {/* <a href={element.url}></a> <p class="card-text">{}</p> */}
+
+                                        <p class="card-text">{element.description}</p>
+                                        <p class="card-text">{element.publishedAt}</p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }))
+                }
+            </div>
         </>
     )
 }
 
 
-export default About;
+export default Sports;
